e6c5e1c9b779efb7270a191daf61c8f59937d458
giufus <giufus@gmail.com>
2024-05-18 07:53:32 +0200

    chpt 3 OK

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,60 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+
+pub struct Order {
+    pub product_name: String,
+    pub quantity: usize,
+    pub unit_price: usize,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: usize, unit_price: usize) -> Self {
+        if product_name.is_empty() || product_name.len() > 300 || quantity == 0 || unit_price == 0 {
+            panic!("Invalid input");
+        }
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &usize {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &usize {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> usize {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("Invalid input");
+        }
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: usize) {
+        if quantity == 0 {
+            panic!("Invalid input");
+        }
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: usize) {
+        if unit_price == 0 {
+            panic!("Invalid input");
+        }
+        self.unit_price = unit_price;
+    }
+}
