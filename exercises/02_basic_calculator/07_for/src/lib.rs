4ea7c5831c5a0222676e4cc9f72ac00219c26b17
giufus <giufus@gmail.com>
2024-12-22 18:26:15 +0100

    fix missing

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,17 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut res: u32 = 1;
+    
+    if n == 0 {
+        return res
+    } 
+    
+    for i in 1..=n {
+        res = res * i;     
+    }
+    
+    res
+
 }
 
 #[cfg(test)]
